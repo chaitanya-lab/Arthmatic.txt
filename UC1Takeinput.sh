3178163e456a579d13ae9666f78fa7fad11a66dd
unknown <you@example.com>
2020-11-24 08:42:22 +0530

    Added third file

--- UC1Takeinput.sh
+++ UC1Takeinput.sh
@@ -2,5 +2,7 @@
 read -p "enter the input first " a
 read -p "enetr the second input " b
 read -p  "enetr the third  input " c
-var=$(($a+$b*$c))
-echo $var
+#var=$(($a+$b*$c))
+var1=$(($a*$b+$c))
+#echo $var
+echo $var1
