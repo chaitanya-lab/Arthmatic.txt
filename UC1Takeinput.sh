69f9438ceeb60951e12d358c25324411bd38f4fa
unknown <you@example.com>
2020-11-24 09:06:23 +0530

    Added new file

--- UC1Takeinput.sh
+++ UC1Takeinput.sh
@@ -2,11 +2,15 @@
 read -p "enter the input first " a
 read -p "enetr the second input " b
 read -p  "enetr the third  input " c
-#var=$(($a+$b*$c))
-#var1=$(($a*$b+$c))
-#var2=$(($c+$a/$b))
+declare -A variable
+var=$(($a+$b*$c))
+var1=$(($a*$b+$c))
+var2=$(($c+$a/$b))
 var3=$(($a%$b+$c))
-#echo $var
-#echo $var1
-#echo $var2
-echo $var3
+
+variable[var]=$var
+variable[var1]=$var1
+variable[var2]=$var2
+variable[var3]=$var3
+echo "values in dictionary :${variable[@]}"
+echo "number of key values :${!variable[@]}"
