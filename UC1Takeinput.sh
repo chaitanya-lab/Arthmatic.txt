3ccf0cf22c3489ea4a1e09f38dee228e7c11b101
unknown <you@example.com>
2020-11-24 08:49:16 +0530

    Added new file

--- UC1Takeinput.sh
+++ UC1Takeinput.sh
@@ -3,6 +3,8 @@ read -p "enter the input first " a
 read -p "enetr the second input " b
 read -p  "enetr the third  input " c
 #var=$(($a+$b*$c))
-var1=$(($a*$b+$c))
+#var1=$(($a*$b+$c))
+var2=$(($c+$a/$b))
 #echo $var
-echo $var1
+#echo $var1
+echo $var2
